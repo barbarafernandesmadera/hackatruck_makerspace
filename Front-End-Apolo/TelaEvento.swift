455e53e6a5d436f77c826af938457a11a7e1eac3
Bárbara Fernandes Madera <108741877+barbarafernandesmadera@users.noreply.github.com>
2024-02-15 10:54:54 -0300

    Add files via upload

---
+++ Front-End-Apolo/TelaEvento.swift
@@ -0,0 +1,20 @@
+//
+//  TelaEvento.swift
+//  TelaComprovante
+//
+//  Created by Student on 29/03/23.
+//
+
+import SwiftUI
+
+struct TelaEvento: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TelaEvento_Previews: PreviewProvider {
+    static var previews: some View {
+        TelaEvento()
+    }
+}
---
+++ Front-End-Apolo/Tela_inicialApp.swift
@@ -0,0 +1,18 @@
+//
+//  Tela_inicialApp.swift
+//  Tela_inicial
+//
+//  Created by Student21 on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Tela_inicialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
